99a28cc94dc42b6b440da59f52e583c49f4d38ce
jugglingdev <kaylamarie1785@gmail.com>
2023-11-02 15:01:22 -0500

    Completed 'Transforming Response Data'

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -21,6 +22,11 @@ export class DataStorageService {
   fetchRecipes() {
     this.http
       .get<Recipe[]>('https://ng-recipe-book-32370-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
