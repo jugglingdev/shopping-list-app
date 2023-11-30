615c1bbaa9abb764600b6b55c0d66bd0d75efd00
jugglingdev <kaylamarie1785@gmail.com>
2023-11-29 22:17:15 -0600

    Completed 'Splitting Modules Correctly'

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,9 @@ import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.compo
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipesComponent } from "./recipes.component";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { RecipesComponent } from "./recipes.component";
     RecipeStartComponent,
     RecipeEditComponent,
   ],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   exports: [
     RecipesComponent,
     RecipeListComponent,
